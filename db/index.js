e3ab499c983996c077eff9b3b14b166c5f00c7e3
Tabu Hana <taboodog@hotmail.com>
2022-04-26 01:13:46 -0700

    trying to move files again

---
+++ db/index.js
@@ -0,0 +1,1 @@
+module.exports = require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/project3_db')
---
+++ routes/index.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+
+//add routes here
+
+
+module.exports = router
