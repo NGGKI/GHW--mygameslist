18aedb627dde1fb3faa438dadce8340de76a3917
Tabu Hana <taboodog@hotmail.com>
2022-04-29 10:32:43 -0700

    base components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
 import Nav from "./components/Nav"
+import Main from "./components/Main"
 function App() {
   return (
     <>
     <Header />
     <Nav />
+    <Main />
     </>
   )
 }
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './header.css'
 
 function Header() {
   return (
-    <div>Header</div>
+    <body>
+      <div class="container">
+        <div class="header">
+          <div class="title">
+            <h1>MyGamesList</h1>
+          </div>
+          <div class="userAccess">
+            <div class="login">Login</div>
+            <div class="sign-up">Sign Up</div>
+          </div>
+        </div>
+      </div>
+    </body>
   )
 }
 
