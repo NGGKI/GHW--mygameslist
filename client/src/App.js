63d68940ae8bc80587990952cfba47b97cc7e726
Tabu Hana <taboodog@hotmail.com>
2022-04-28 19:16:46 -0700

    working on design of frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header"
+import Nav from "./components/Nav"
 function App() {
   return (
-    <div>hi</div>
+    <>
+    <Header />
+    <Nav />
+    </>
   )
 }
 
---
+++ design/README.md
@@ -0,0 +1,3 @@
+## Open using live server
+```
+This isn't connected to anything. Please use this only as a reference on what to do for the react pages!
